36ef29da0cdc502fadc03fea1870c1f4f90cb9dd
Sai pradeep <93672106+XpradeepX@users.noreply.github.com>
2022-05-01 10:37:42 +0530

    Add files via upload

---
+++ arithematic operators.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{int a,b,SUM,diff,product,mod,div;
+printf("enter value of a=");
+scanf("%d",&a);
+printf("enter value of b=");
+scanf("%d",&b);
+SUM=a+b;
+diff=a-b;
+product=a*b;
+mod=a%b;
+div=a/b;
+printf("************************");
+printf("\nthe sum is=%d",SUM);
+printf("\nthe diff is=%d",diff);
+printf("\nthe product is= %d",product);
+printf("\nthe division is= %d",div);
+
+}
+
